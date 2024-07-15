666e6215c41308e9aad8dc17a590605a9d58b058
giorgi-2001 <gio19900504@gmail.com>
2024-07-15 16:07:35 +0400

    Implemented substructure search

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
+from rdkit import Chem
+
+
 def substructure_search(mols, mol):
-    pass
+    return [test_mol for test_mol in mols if Chem.MolFromSmiles(test_mol).HasSubstructMatch(Chem.MolFromSmiles(mol))]
+
+        
+print(substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1"))
